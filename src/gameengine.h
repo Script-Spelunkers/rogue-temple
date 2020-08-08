deb46bb3c4573f6f075ea8260e53ee70fcf8be01
Justin <contact@tsujin.com>
2020-08-08 19:34:39 -0400

    added gamestate/engine objects
    Added the GameEngine as well as an abstract GameState class. currently no concrete states exist, but this skeleton should make it very simple to add them.

---
+++ src/gameengine.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <vector>
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+
+class GameState;
+
+class GameEngine {
+public:
+	void Start(const char* title, int width = 640, int height = 480,
+		       bool fullscreen = false);
+	void Cleanup();
+
+	void ChangeState(GameState* state);
+	void PushState(GameState* state);
+	void PopState();
+
+	void HandleEvents();
+	void Update();
+	void Draw();
+
+	bool Running() { return m_running; }
+	void Quit() { m_running = false; }
+
+	sf::RenderWindow* window;
+
+private:
+	// our state stack
+	vector<GameState*> states;
+
+	bool m_running;
+	bool m_fullscreen;
+};
