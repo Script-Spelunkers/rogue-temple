2f649287fd788f354193c6cb218b2d12f944a044
Justin <contact@tsujin.com>
2020-08-30 14:58:26 -0400

    gave Entity an _x and_y position
    updated Entity to have x/y positions and fixed trivial constructor for Entity. also deleted the useless TextureRect for the player sprite

--- src/entity.h
+++ src/entity.h
@@ -1,11 +1,10 @@
 #pragma once
-
 #include "SFML/Graphics.hpp"
 
 class Entity : public sf::Drawable {
 public:
-    Entity() = default;
-    explicit Entity(sf::Sprite& sprite) : _sprite(sprite) {};
+    Entity() {};
+    explicit Entity(sf::Sprite& sprite, int x, int y) : _sprite(sprite), _x(x), _y(y) {};
     virtual void draw(sf::RenderTarget&, sf::RenderStates) const = 0;
     virtual void update() = 0;
 
@@ -14,5 +13,8 @@ public:
 protected:
     sf::Texture _texture;
     sf::Sprite _sprite;
+
+    int _x;
+    int _y;
 
 };
